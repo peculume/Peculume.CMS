99b97c28ced3314e82d78585d8fb3e6bbafc010b
Niall Lewin <lewin91@hotmail.com>
2025-05-07 14:48:40 +0100

    Added image loading

--- src/pages/MediaItemPage/MediaItemPage.tsx
+++ src/pages/MediaItemPage/MediaItemPage.tsx
@@ -1,12 +1,13 @@
 import { useNavigate, useParams } from "react-router";
 import { useGetMediaById } from "hooks/MediaHooks/MediaHooks";
+import { useState } from "react";
 import styles from "./MediaItemPage.module.scss";
 
 const MediaItemPage = () => {
   const { mediaId } = useParams();
   const navigate = useNavigate();
-
   const { media } = useGetMediaById(mediaId);
+  const [isLoading, setIsLoading] = useState(true);
 
   if (!media) {
     return null;
@@ -19,7 +20,13 @@ const MediaItemPage = () => {
         <h2 className={styles.title}>{media.name}</h2>
       </div>
       <div className={styles.imageContainer}>
-        <img src={media.url} alt={media.name} className={styles.image} />
+        {isLoading && <div className={styles.spinner}>Loading...</div>}
+        <img
+          src={media.url}
+          alt={media.name}
+          className={`${styles.image} ${isLoading ? styles.hidden : ""}`}
+          onLoad={() => setIsLoading(false)}
+        />
       </div>
     </div>
   );
